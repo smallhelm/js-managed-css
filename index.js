a345d123bbcc2ea6fd1fe72ab2047b8902cd9543
Matthew Wright <matthew@smallhelm.com>
2015-12-19 08:55:11 -0700

    batch insert the initial css code

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var jsToCss = require("./js-to-css");
 var insertCss = require("insert-css");
 
+var css = "";
+var has_inserted_batch = false;
+setTimeout(function(){
+  if(css.length > 0){
+    insertCss(css);
+    has_inserted_batch = true;
+  }
+}, 1);
+
 module.exports = function(json){
   var d = jsToCss(json);
-  insertCss(d.css);
+  if(has_inserted_batch){
+    insertCss(d.css);
+  }else{
+    css += "\n" + d.css;
+  }
   return d.vars;
 };
